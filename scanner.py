6b9f09c89d07c25f284c746a298b35923bec227b
Rama krishna <ramakrishna.polumuru@gmail.com>
2017-07-06 10:33:39 +0530

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,12 +1,46 @@
+#import Tkinter
+
 import subprocess
 import socket
 import os
-with open(os.devnull, "wb") as limbo:
-        for n in xrange(10, 240):
-                ip='10.4.16.{0}'.format(n)
+#class simpleapp_tk(Tkinter.Tk):
+ #   def __init__(self,parent):
+  #      Tkinter.Tk.__init__(self,parent)
+   #     self.parent = parent
+    #    self.initialize()
+    #def initialize(self):
+     #   self.grid()
+      #  self.entry = Tkinter.Entry(self)
+       # self.entry.grid(column=0,row=0)
+
+
+
+
+
+def lookup(addr):
+     try:
+         return socket.gethostbyaddr(addr)
+     except socket.herror:
+         return None, None, None
+def ARP_Scan(ip_add_range):
+    with open(os.devnull, "wb") as limbo:
+        for n in xrange(1, 200):
+                ip=ip_add_range.format(n)
                 result=subprocess.Popen(["ping", "-n", "1", "-w", "200", ip],
                         stdout=limbo, stderr=limbo).wait()
-                if result:
+                if  result:
                         print (ip, "inactive")
                 else:
-                        print (ip, "active")
+
+                        name,alias,addresslist = lookup(ip)
+                        if name != None:
+                            name1 = name
+                            print (ip, "active",name1)
+
+
+
+ARP_Scan("10.4.16.{0}")
+#if __name__ =="__main__":
+ #   app = simpleapp_tk(None)
+  #  app.title('my application')
+   # app.mainloop()
